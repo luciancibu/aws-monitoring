43ba759223e481c37e3e77ddbef426ef096fa09d
Lucian Cibu <luciancibu@yahoo.com>
2025-12-11 21:39:32 +0200

    Prepare infrastructure for ansible

---
+++ terraform/outputs.tf
@@ -0,0 +1,38 @@
+# Public IPs
+output "ansible_public_ip" {
+  description = "Public IP of the Ansible"
+  value       = aws_instance.ansible_ec2.public_ip
+}
+
+# Private IPs 
+output "flask_private_ip" {
+  description = "Private IP of the Python Flask EC2"
+  value       = aws_instance.python_flask.private_ip
+}
+
+# Ansible Inventory File
+resource "local_file" "python_flask" {
+  filename = "../ansible/inventory"
+
+  content = templatefile("${path.module}/templates/inventory.tmpl", {
+    python_ip  = aws_instance.python_flask.private_ip
+    python_user  = var.ansibleUserByOS[aws_instance.python_flask.tags.os]
+
+    clientkey  = aws_key_pair.monitoringKeypair.key_name
+    
+    depends_on = [
+    aws_instance.python_flask,
+  ]
+  })
+}
+
+# Deployment script for Ansible
+resource "local_file" "ansible_deployment" {
+  filename = "../${var.deployName}"
+
+  content = templatefile("${path.module}/templates/deploy.tmpl", {
+    ansible_ip       = aws_instance.ansible_ec2.public_ip
+    ansible_user     = var.ansibleUserByOS[aws_instance.ansible_ec2.tags.os]
+    clientkey        = "${aws_key_pair.monitoringKeypair.key_name}.pem"
+  })
+}
