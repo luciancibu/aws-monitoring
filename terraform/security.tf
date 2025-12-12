fd33cb602641f5bdae2b1b49310f207c1a6f5c00
Lucian Cibu <luciancibu@yahoo.com>
2025-12-12 06:04:54 +0200

    Use security gorup instead of IPs for ingress rules

--- terraform/security.tf
+++ terraform/security.tf
@@ -39,15 +39,15 @@ resource "aws_security_group" "flask_sg" {
   vpc_id      = data.aws_vpc.default_vpc.id
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_instance.ansible_ec2.private_ip}/32"]
+    description     = "SSH from Ansible EC2"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ansible_sg.id]
   }
 
   ingress {
-    description = "SSH"
+    description = "SSH from my laptop"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -59,7 +59,7 @@ resource "aws_security_group" "flask_sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    cidr_blocks = ["${var.myIP}/32"]
+    cidr_blocks      = ["${var.myIP}/32"]
   }  
   
   egress {
@@ -77,12 +77,15 @@ resource "aws_security_group" "flask_sg" {
 
 resource "aws_security_group" "rds_sg" {
   name = "${var.projectName}-rds-sg"
+  description = "Security group for RDS"
+  vpc_id     = data.aws_vpc.default_vpc.id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "MySQL from Flask EC2"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.flask_sg.id]
   }
 
   egress {
@@ -90,5 +93,10 @@ resource "aws_security_group" "rds_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.projectName}-rds-sg"
+    Project = var.projectName
   }
 }
