0f2ff71d80053f4e3d38629436b700fecfbfff2d
Lucian Cibu <luciancibu@yahoo.com>
2025-12-13 03:18:00 +0200

    Use existing Secrets Manager secret for RDS credentials

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,7 +1,7 @@
-resource "random_password" "mysql_pass" {
-  length  = 24
-  special = false
-}
+# resource "random_password" "mysql_pass" {
+#   length  = 24
+#   special = false
+# }
 
 resource "aws_db_instance" "mysql" {
   identifier = "${var.projectName}-mysql"
@@ -10,34 +10,54 @@ resource "aws_db_instance" "mysql" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t4g.micro"
-  username             = "rds_username"
-  password = random_password.mysql_pass.result
+  # username             = "rds_username"
+  # password = random_password.mysql_pass.result
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   publicly_accessible   = true
+
+  username = jsondecode(
+          data.aws_secretsmanager_secret_version.mysql.secret_string
+        )["username"]
+
+  password = jsondecode(
+          data.aws_secretsmanager_secret_version.mysql.secret_string
+        )["password"]
+
 }
 
-resource "aws_secretsmanager_secret" "mysql_secret" {
+data "aws_secretsmanager_secret" "mysql" {
   name = "${var.projectName}-mysql-secret"
-  recovery_window_in_days = 7
+}
 
-  lifecycle {
-    prevent_destroy = true
-  }
+data "aws_secretsmanager_secret_version" "mysql" {
+  secret_id = data.aws_secretsmanager_secret.mysql.id
+}
+
+
+# resource "aws_secretsmanager_secret" "mysql_secret" {
+#   name = "${var.projectName}-mysql-secret"
+#   recovery_window_in_days = 7
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
   
-  tags = {
-    Project = var.projectName
-  }
-}
+#   tags = {
+#     Project = var.projectName
+#   }
+# }
 
-resource "aws_secretsmanager_secret_version" "mysql_secret_version" {
-  secret_id = aws_secretsmanager_secret.mysql_secret.id
+# resource "aws_secretsmanager_secret_version" "mysql_secret_version" {
+#   secret_id = aws_secretsmanager_secret.mysql_secret.id
 
-  secret_string = jsonencode({
-    username = "rds_username"
-    password = random_password.mysql_pass.result
-    host     = aws_db_instance.mysql.address
-    port     = 3306
-    dbname   = "appdb"
-  })
-}
+#   secret_string = jsonencode({
+#     username = "rds_username"
+#     password = random_password.mysql_pass.result
+#     host     = aws_db_instance.mysql.address
+#     port     = 3306
+#     dbname   = "appdb"
+#   })
+# }
+
+
