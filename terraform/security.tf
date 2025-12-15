2309d710ab0147fedb3f6b9e6eed7aee330503c7
Lucian Cibu <luciancibu@yahoo.com>
2025-12-15 14:22:07 +0200

    Allow public HTTP access to web app (port 80)

--- terraform/security.tf
+++ terraform/security.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "flask_sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    cidr_blocks      = ["${var.myIP}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   } 
 
   ingress {
